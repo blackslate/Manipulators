4fff2483ba0d159957358dab3c93b2fc01f33d78
James Newton <blackslate@lexogram.com>
2015-12-03 13:11:13 -0500

    World translation in all bases now working.

--- planeRayIntersection/js/custom.js
+++ planeRayIntersection/js/custom.js
@@ -21,47 +21,112 @@ of the manipulated object. Later versions will:
 function init() {
   var scene = new THREE.Scene()
 
+  // <FOR TESTING ONLY>
+  var freedom = "z" //"x" | "y" | "z" | "yz" | "xy" | "xz" | "xyz"
+  var basis = "world" // "local" | "view"
+  var action = "translate"
+
+  var dragCube
+
+  document.querySelector("#basis").onchange = function (event) {
+    basis = event.target.value
+  }
+  document.querySelector("#action").onchange = function (event) {
+    action = event.target.value
+  }
+
+  ;(function createWorldPlanes(){
+    // create the ground plane
+    var planeGeometry = new THREE.PlaneGeometry(1000, 1000)
+    var yellowMaterial = new THREE.MeshBasicMaterial({
+      color: 0xffff00
+    , opacity: 0.25
+    , transparent: true
+    })
+    var cyanMaterial = new THREE.MeshBasicMaterial({
+      color: 0x00ffff
+    , opacity: 0.5
+    , transparent: true
+    })
+    var magentaMaterial = new THREE.MeshBasicMaterial({
+      color: 0xff00ff
+    , opacity: 0.5
+    , transparent: true
+    })
+    var xyPlane = new THREE.Mesh(planeGeometry, yellowMaterial)
+    scene.add(xyPlane)
+
+    var yzPlane = new THREE.Mesh(planeGeometry, cyanMaterial)
+    yzPlane.rotation.y = 0.5 * Math.PI
+    scene.add(yzPlane)
+
+    var xzPlane = new THREE.Mesh(planeGeometry, magentaMaterial)
+    xzPlane.rotation.x = -0.5 * Math.PI
+    scene.add(xzPlane)
+  })()
+ 
+  ;(function treatContext(){
+    // Use Z, X and C (instead of Y) or ;QJ to toggle freedoms 
+    window.addEventListener( 'keyup', toggleFreedom, false )
+
+    var freedomElement = document.querySelector("#freedom")
+    var keys = {
+      88: "x"
+    , 67: "y" // c
+    , 86: "y" // v
+    , 89: "y"
+    , 90: "z"
+      // DVORAK
+    , 81: "x" // q
+    , 74: "y" // j
+    , 75: "y" // k
+    , 186: "z" // ;
+    }
+    var freedomKeysPressed = []
+
+    function toggleFreedom(event) {
+      var key = keys[event.keyCode]
+
+      if (key) {
+        var index = freedomKeysPressed.indexOf(key)
+
+        if (index < 0) {
+          freedomKeysPressed.push(key)
+          freedomKeysPressed.sort()
+          freedom = freedomKeysPressed.join("")
+        } else {
+          freedomKeysPressed.splice(index, 1)
+          freedom = freedomKeysPressed.join("")
+        }
+
+        freedomElement.innerHTML = freedom || "xyz"
+      }
+    }
+  })()
+
+  ;(function testing() {     
+    // CUBE TO DRAG
+    var cubeGeometry = new THREE.BoxGeometry(40, 40, 40)
+    var cubeMaterial = new THREE.MeshBasicMaterial({
+      color: 0x000000
+    , wireframe: true})
+    dragCube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+    dragAxes = new THREE.AxisHelper(100)
+    dragCube.add(dragAxes)
+    dragCube.rotation.x = 30
+    dragCube.rotation.y = 45
+    scene.add(dragCube)
+  })()
+  // </FOR TESTING ONLY>
+
   // Create a perspective camera
-  var viewAngle = 45
-  var width = window.innerWidth
-  var height = window.innerHeight
-  var ratio = width / height
-  var near = 0.1
-  var far = 3000
-  var camera = new THREE.PerspectiveCamera(viewAngle,ratio,near,far)
-
-  // create a render and set the size
-  var renderer = new THREE.WebGLRenderer()
-  renderer.setClearColor(new THREE.Color(0xEEEEEE))
-  renderer.setSize(width, height)
-
-  // create the ground plane
-  var planeGeometry = new THREE.PlaneGeometry(1000, 1000)
-  var yellowMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffff00
-  , opacity: 0.25
-  , transparent: true
-  })
-  var cyanMaterial = new THREE.MeshBasicMaterial({
-    color: 0x00ffff
-  , opacity: 0.5
-  , transparent: true
-  })
-  var magentaMaterial = new THREE.MeshBasicMaterial({
-    color: 0xff00ff
-  , opacity: 0.5
-  , transparent: true
-  })
-  var xyPlane = new THREE.Mesh(planeGeometry, yellowMaterial)
-  scene.add(xyPlane)
-
-  var yzPlane = new THREE.Mesh(planeGeometry, cyanMaterial)
-  yzPlane.rotation.y = 0.5 * Math.PI
-  scene.add(yzPlane)
-
-  var xzPlane = new THREE.Mesh(planeGeometry, magentaMaterial)
-  xzPlane.rotation.x = -0.5 * Math.PI
-  scene.add(xzPlane)
+  var FOV = 45
+  var WIDTH = window.innerWidth
+  var HEIGHT = window.innerHeight
+  var ASPECT = WIDTH / HEIGHT
+  var NEAR = 0.1
+  var FAR = 3000
+  var camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR)
 
   // position and point the camera to the center of the scene
   camera.position.x = 500
@@ -69,228 +134,202 @@ function init() {
   camera.position.z = 1300
   camera.lookAt(scene.position)
 
+  // create a render and set the size
+  var renderer = new THREE.WebGLRenderer()
+  renderer.setClearColor(new THREE.Color(0xEEEEEE))
+  renderer.setSize(WIDTH, HEIGHT)
+
+
   // add the output of the renderer to the html element
   document.getElementById("WebGL-output").appendChild(renderer.domElement)
 
   ;(function createObjectManipulator(scene, camera){
-    var freedom = "" //"x" | "y" | "z" | "yz" | "xy" | "xz" | "xyz"
-    var space = "world" // "local" | "view"
-    var action = "translate"
+    window.addEventListener( 'mousedown', startDrag, false )
 
-     // <FOR TESTING ONLY>
-    var dragPlane
-    var dragCube
-    var zAxis
-    var freedomElement = document.querySelector("#freedom")
-    document.querySelector("#space").onchange = function (event) {
-      space = event.target.value
-    }
-    document.querySelector("#action").onchange = function (event) {
-      action = event.target.value
-    }
+    var cameraPosition = new THREE.Vector3()
+    var axisPoint = new THREE.Vector3()
+    var nearPoint = new THREE.Vector3(0, 0, -1)
+    var farPoint = new THREE.Vector3(0, 0, 1)
+    var planePoint = new THREE.Vector3()
+    var basisMatrix = new THREE.Matrix4() // world space
+    var xAxis = new THREE.Vector3()
+    var yAxis = new THREE.Vector3()
+    var zAxis = new THREE.Vector3()
+    var plane = new THREE.Plane()
+    var moveRay = new THREE.Ray()
+    var mouseRay = new THREE.Ray()
+    var viewToWorldMatrix = new THREE.Matrix4()
 
-    ;(function treatContext(){
-      // Use Z, X and C (instead of Y) or ;QJ to toggle freedoms 
-      window.addEventListener( 'keyup', toggleFreedom, false )
-      var keys = {
-        88: "x"
-      , 67: "y" // c
-      , 86: "y" // v
-      , 89: "y"
-      , 90: "z"
-        // DVORAK
-      , 81: "x" // q
-      , 74: "y" // j
-      , 75: "y" // k
-      , 186: "z" // ;
-      }
-      var freedomKeysPressed = []
+    var targetData
+      , targetObject
 
-      function toggleFreedom(event) {
-        var key = keys[event.keyCode]
+    function startDrag(event) { 
+      window.addEventListener( 'mousemove', drag, false )
+      window.addEventListener( 'mouseup', stopDrag, false )
 
-        if (key) {
-          var index = freedomKeysPressed.indexOf(key)
+      targetData = getModelUnderMouse(event) // uses Raycaster
 
-          if (index < 0) {
-            freedomKeysPressed.push(key)
-            freedomKeysPressed.sort()
-            freedom = freedomKeysPressed.join("")
-          } else {
-            freedomKeysPressed.splice(index, 1)
-            freedom = freedomKeysPressed.join("")
-          }
-
-          freedomElement.innerHTML = freedom || "xyz"
-        }
-      }
-    })()
-
-    ;(function testing() {     
-      // CUBE TO DRAG
-      var cubeGeometry = new THREE.BoxGeometry(40, 40, 40)
-      var cubeMaterial = new THREE.MeshBasicMaterial({
-        color: 0x000000
-      , wireframe: true})
-      dragCube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-      dragCube.rotation.x = 30
-      dragCube.rotation.y = 45
-    })()
-    // </FOR TESTING ONLY>
-
-    var viewport = window
-    var viewWidth = viewport.innerWidth
-    var viewHeight = viewport.innerHeight
-
-    ;(function cameraProjectDemo(){
-      viewport.addEventListener( 'mousedown', startDrag, false )
-
-      var targetData
-        , worldClickPoint
-        , viewClickPoint
-        , cameraPosition // set on startDrag
-
-      function startDrag(event) { 
-        window.addEventListener( 'mousemove', drag, false )
-        window.addEventListener( 'mouseup', stopDrag, false )
- 
-        cameraPosition = camera.getWorldPosition()
-        var viewPoint = getViewPoint(event)
-        targetData = getModelUnderMouse(viewPoint) // uses Raycaster
-
-        if (!targetData) {
-          stopDrag()
-          return
-        }
-
-        worldClickPoint = targetData.point
-        viewClickPoint = worldClickPoint.clone().project(camera)
-
-        ;(function testing(){ 
-          dragCube.position.copy(targetData.point)
-          scene.add(dragCube)
-        })()
+      if (!targetData) {
+        stopDrag()
+        return
       }
 
-      function drag(event) {
-        switch (action) {
-          // IGNORE action FOR NOW. JUST TRANSLATE.
-          case "rotate":
-          case "scale":
-          case "translate":
-            dragToTranslate(event)
-          break
-        }
+      targetObject = targetData.object
+      setBasis()
+
+      
+      viewToWorldMatrix.multiplyMatrices(camera.matrixWorld, viewToWorldMatrix.getInverse(camera.projectionMatrix))
+
+      switch (action) {
+        // IGNORE action FOR NOW. JUST TRANSLATE.
+        case "rotate":
+        case "scale":
+        case "translate":
+          initializeTranslation(event)
+        break
       }
 
-      function dragToTranslate (event) {
-        var matrix = new THREE.Matrix4() // world space
-        var xAxis = new THREE.Vector3()
-        var yAxis = new THREE.Vector3()
-        var zAxis = new THREE.Vector3()
-
-        switch (space) {
-          case "view":
-            matrix.copy(camera.matrixWorld)
-          break
-          case "local":
-            matrix.copy(dragCube.matrixWorld)
-          break
-          case "world":
-            // matrix is already in world space
-          break
-        }
-
-        matrix.extractBasis(xAxis,yAxis,zAxis)
+      function initializeTranslation() {
+        axisPoint.setFromMatrixPosition(targetObject.matrixWorld)
 
         switch (freedom) {
           default: // case "xyz":
-            // No constraint: drag along plane parallel to viewport
-            constrainToViewPlane(event)
+            createConstraintPlane(camera.getWorldDirection())
           break
 
           case "x":
-            constrainToAxis(xAxis, event)
+            createAxisPlane(xAxis)
           break
           case "y":
-            constrainToAxis(yAxis, event)
+            createAxisPlane(yAxis)
           break
           case "z":
-           constrainToAxis(zAxis, event)
+            createAxisPlane(zAxis)
           break
 
           case "yz":
-            constrainToPlane("x", xAxis, event)
+            createConstraintPlane(xAxis)
           break
           case "xz":
-            constrainToPlane("y", yAxis, event)
+            createConstraintPlane(yAxis)
           break
           case "xy":
-            constrainToPlane("z", zAxis, event)
+            createConstraintPlane(zAxis)
           break
         }
 
-        function constrainToAxis(axis, event) {
-          var axisRay = new THREE.Ray(
-            dragCube.getWorldPosition()
-          , axis
-          )
-          var viewRay = getCameraRay(event)
-          var axisPosition = axisRay.closestPointToRay(viewRay)
-          if (axisPosition) {
-            dragCube.position.copy(axisPosition)
-          }
-        }
+        function createAxisPlane(translationAxis) {
+          moveRay.direction.copy(translationAxis)
+          moveRay.origin.copy(axisPoint)
 
-        function constrainToPlane(constrain, normal, event) {
+          cameraPosition.setFromMatrixPosition( camera.matrixWorld )
           
+          moveRay.closestPointToPoint(cameraPosition, axisPoint)
+          plane.normal.subVectors(cameraPosition, axisPoint)
+
+          //// TREAT SPECIAL CASE:
+          //// ** axisPoint identical to cameraPosition **
+          //// This is acceptable for an orthogonal camera unless
+          //// the translation axis is parallel to the camera 
+          //// direction, but it is always meaningless for a
+          //// perspective camera
+          if (plane.normal.length() < Number.EPSILON) {
+            // TODO: Check for orthogonal camera
+            return stopDrag()
+          }
+          
+          plane.normal.normalize()
+          plane.constant = -axisPoint.dot(plane.normal)
         }
 
-        function constrainToViewPlane(event) {
-          var worldPoint = getWorldViewPoint(event, viewClickPoint.z)
-          dragCube.position.copy(worldPoint)
+        function createConstraintPlane(normal) {
+          plane.normal.copy(normal).normalize()
+          plane.constant = -axisPoint.dot(plane.normal)
         }
       }
+    }
 
-      function stopDrag(event) {
-        window.removeEventListener( 'mousemove', drag, false )
-        window.removeEventListener( 'mouseup', stopDrag, false )      
+    function setBasis() {
+      switch (basis) {
+        case "view":
+          basisMatrix.copy(camera.matrixWorld)
+        break
+        case "local":
+          basisMatrix.copy(targetObject.matrixWorld)
+        break
+        case "world":
+          basisMatrix.identity()
+        break
       }
 
-      function getViewPoint(event, z) {
-        var viewPoint = new THREE.Vector3()
+      basisMatrix.extractBasis(xAxis,yAxis,zAxis)
+    }
+  
+    function drag(event) {
+      switch (action) {
+        // IGNORE action FOR NOW. JUST TRANSLATE.
+        case "rotate":
+        case "scale":
+        case "translate":
+          dragToTranslate(event)
+        break
+      }
+    }
 
-        viewPoint.x = (event.clientX / viewWidth) * 2 - 1
-        viewPoint.y = 1 - (event.clientY / viewHeight) * 2
-        if (z) {
-          viewPoint.z = z
-        }
- 
-        return viewPoint
+    function dragToTranslate(event) {
+      // setBasis()
+      setMouseRay(event)
+      mouseRay.intersectPlane(plane, planePoint)
+
+      switch (freedom) {
+        case "x":
+        case "y":
+        case "z":
+          planePoint.mapToRayLine(moveRay)
+        break
       }
 
-      function getWorldViewPoint(event, z) {
-        var viewPoint = getViewPoint(event, z)
-        viewPoint.unproject(camera)
-        return viewPoint
-      }
+      // TESTING 
+      dragCube.position.copy(planePoint)    
+    }
 
-      function getCameraRay(event) {
-        var worldPoint = getWorldViewPoint(event)
-        var direction = worldPoint.sub(cameraPosition).normalize()
-        var ray = new THREE.Ray(cameraPosition, direction)
-        return ray
-      }
+    function stopDrag(event) {
+      window.removeEventListener( 'mousemove', drag, false )
+      window.removeEventListener( 'mouseup', stopDrag, false )      
+    }
 
-      function getModelUnderMouse(viewPoint) {
-        var raycaster = new THREE.Raycaster()    
+    function setMouseRay(event) {
+      getViewPoint(event, nearPoint)
+      farPoint.copy(nearPoint).z = 1
+      nearPoint.setZ(-1).applyProjection(viewToWorldMatrix)
+      farPoint.applyProjection(viewToWorldMatrix)
+      
+      mouseRay.origin.copy(nearPoint)
+      mouseRay.direction.subVectors(farPoint, nearPoint)
+    }
 
-        raycaster.setFromCamera( viewPoint, camera )
-        intersects = raycaster.intersectObjects(scene.children)
-        
-        return intersects[0] // may be undefined
-      }
-    })()
+    function getViewPoint(event, viewPoint) {
+      viewPoint ? null : viewPoint = new THREE.Vector3()
+
+      viewPoint.x = (event.clientX / WIDTH) * 2 - 1
+      viewPoint.y = 1 - (event.clientY / HEIGHT) * 2
+      // viewPoint.z = 0
+       
+      return viewPoint      
+    }
+
+    function getModelUnderMouse(event) {
+      // TO BE REPLACED BY A registerTool click EVENT
+      var viewPoint = getViewPoint(event)
+
+      var raycaster = new THREE.Raycaster()    
+
+      raycaster.setFromCamera( viewPoint, camera )
+      intersects = raycaster.intersectObject(dragCube)
+      
+      return intersects[0] // may be undefined
+    }
+  
   })(scene, camera)
 
   ;(function render() {
@@ -299,107 +338,11 @@ function init() {
   })()
 }
 
-
-// Extending Three.js //
-
-THREE.Ray.prototype.closestPointToRay = function (that, details) {
-  // that: THREE.Ray() 
-  // details: (optional) object
-  // { pointOnThisRay: <THREE.Vector3>
-  // , pointOnThatRay: <THREE.Vector3>
-  // , midPoint: <THREE.Vector3>
-  // , distanceBetweenClosestPoints: <float>
-  // }
-  // For an explanation of the vector mathematics, see: 
-  // http://morroworks.com/Content/Docs/Rays%20closest%20point.pdf
-  
-  // @return undefined if rays are invalid or parallel
-  //         or THREE.Vector3() point on this ray which is closest
-  //         to that ray.
-  
-  if (!(that instanceof THREE.Ray)) {
-    return
-  }
-  
-  var thisDirection = this.direction
-  var thatDirection = that.direction
-
-  if (!thisDirection.clone().cross(thatDirection).length()) {
-    // Rays are parallel
-    return 
-  }
-
-  if ( !thisDirection.dot(thisDirection)
-    || !thatDirection.dot(thatDirection)) {
-    // At least one of the rays is just a point with no direction
-    return
-  }
-
-  var closestPoint = new THREE.Vector3()
-  var thisOrigin = this.origin
-  var thatOrigin = that.origin
-  var sameOrigin = thisOrigin.equals(thatOrigin)
-
-  if (sameOrigin) {
-    // Simple case
-    closestPoint.copy(thisOrigin)
-  } else {
-    var a = thisDirection.clone().normalize()
-    var b = thatDirection.clone().normalize()
-    var c = thatOrigin.clone().sub(thisOrigin)
-
-    var p = a.dot(b)
-    var q = a.dot(c)
-    var r = b.dot(c)
-    var s = a.dot(a) // already known to be non-zero
-    var t = b.dot(b) // already known to be non-zero
-
-    var divisor = (s * t - p * p)
-
-    if (!divisor) {
-      // The two rays are colinear. They are "closest" at all points
-      // This case should already have been excluded by the .cross()
-      // check made at the start.
-      return
-    }
-
-    var d = (q * t - p * r) / divisor
-    closestPoint.copy(thisOrigin).add(a.multiplyScalar(d))
-  }
-
-  if ( typeof details === "object" ) {
-    details.pointOnThisRay = closestPoint
-    
-    if (sameOrigin) {
-      // Should all points be the same object or clones?
-      details.pointOnThatRay = closestPoint
-      details.midPoint = closestPoint
-      details.distanceBetweenClosestPoints = 0
-    } else {
-      // TODO: Add other details
-      d = (p * q - r * s) / divisor
-      var thatPoint = new THREE.Vector3().copy(thatOrigin).add(b.multiplyScalar(d))
-      details.pointOnThatRay = thatPoint
-      details.midPoint = closestPoint.clone()
-                                     .add(thatPoint)
-                                     .divideScalar(2)
-      details.distanceBetweenClosestPoints = closestPoint.distanceTo(thatPoint)
-    }
-  }
-
-  return closestPoint
-}
-
-THREE.Object3D.prototype.getWorldPosition = function (forceUpdate) {
-  var position = new THREE.Vector3()
-
-  if (forceUpdate) {
-    this.updateMatrixWorld()
-  }
-
-  position.setFromMatrixPosition( this.matrixWorld )
-
-  return position
+THREE.Vector3.prototype.mapToRayLine = function ( ray ) {
+  this.sub( ray.origin )
+  var directionDistance = this.dot( ray.direction );
+  this.copy( ray.direction ).multiplyScalar( directionDistance )
+       .add( ray.origin )
 }
 
 window.onload = init
